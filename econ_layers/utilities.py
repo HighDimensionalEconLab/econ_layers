2dfe84689af14d5b197235d0324cb722f18643b6
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-10-01 10:15:12 -0700

    initial utilities

---
+++ econ_layers/utilities.py
@@ -0,0 +1,5 @@
+def squeeze_cpu(x):
+    return x.detach().squeeze().cpu().numpy() if torch.is_tensor(x) else x
+  
+def dict_to_cpu(d):
+    return {name: squeeze_cpu(val).tolist() for (name, val) in d.items()}
