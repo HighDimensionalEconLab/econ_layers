8a98d946ad1c53faaf8a19ab8800f55b14acaad7
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-10-14 00:40:32 -0700

    add unit test for FlexibleSequential with rescale

--- tests/test_flexible_sequential.py
+++ tests/test_flexible_sequential.py
@@ -9,7 +9,7 @@ import numpy.testing
 import torch.autograd.gradcheck
 from torch.autograd import Variable
 
-from econ_layers.layers import FlexibleSequential
+from econ_layers.layers import FlexibleSequential, ScalarExponentialRescale
 
 torch.set_printoptions(16)  # to be able to see what is going on
 
@@ -30,5 +30,13 @@ def test_simple_flexible_derivative_softplus():
     n_in = 20
     n_out = 3
     mod = FlexibleSequential(n_in, n_out, layers=3, hidden_dim=128, LastActivator=nn.Softplus, last_activator_kwargs={"beta": 2.0} ).double()
+    input = (Variable(torch.randn(n_in).double(), requires_grad=True),)
+    assert(torch.autograd.gradcheck(mod, input))
+    
+# Unit test with an integrated scalar rescale layer in the FlexibleSequential
+def test_simple_flexible_derivative_rescale():
+    n_in = 20
+    n_out = 3
+    mod = FlexibleSequential(n_in, n_out, layers=3, hidden_dim=128, RescalingLayer = ScalarExponentialRescaling).double()
     input = (Variable(torch.randn(n_in).double(), requires_grad=True),)
     assert(torch.autograd.gradcheck(mod, input))
