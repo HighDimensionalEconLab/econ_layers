2e44ee2b6707ecf4d1952955638fb6f6fc56ac57
Jan Rosa <jan.rosa1993@gmail.com>
2021-11-07 18:28:04 -0800

    correcting inputrescaleing

--- econ_layers/layers.py
+++ econ_layers/layers.py
@@ -7,27 +7,32 @@ class InputRescaling(nn.Module):
     def __init__(self, rescale_index):
         super().__init__()
         self.rescale_index = rescale_index
-        
+
     def forward(self, x, y):
-        return y * x[self.rescale_index]
-    
+        return x[:, [self.rescale_index]] * y
+
+
+# [[z_0,k_0], [z]]
+
 
 # Scalar rescaling.  Only one parameter.
 class ScalarExponentialRescaling(nn.Module):
-    def __init__(self, n_in = 1):
+    def __init__(self, n_in=1):
         super().__init__()
         self.n_in = n_in
-        self.weight = torch.nn.Parameter(torch.Tensor(1)) # only one parameter to "learn"
+        self.weight = torch.nn.Parameter(
+            torch.Tensor(1)
+        )  # only one parameter to "learn"
         self.reset_parameters()
-        
+
     def reset_parameters(self):
         # Lets start at zero, but later could have option
         torch.nn.init.zeros_(self.weight)
-    
+
     def forward(self, x, y):
         exp_x = torch.exp(self.weight * x)  # exponential of input
         return torch.mul(exp_x, y)
-        
+
 
 # There is no exponential layer in pytorch, so this adds one.
 class Exponential(nn.Module):
@@ -58,7 +63,7 @@ class FlexibleSequential(nn.Module):
         last_activator_kwargs={},
         last_bias=True,
         RescalingLayer=None,
-        rescaling_layer_kwargs={}
+        rescaling_layer_kwargs={},
     ):
         """
         Init method.
@@ -75,10 +80,12 @@ class FlexibleSequential(nn.Module):
         self.hidden_bias = hidden_bias
         self.last_bias = last_bias
         self.rescaling_layer_args = rescaling_layer_kwargs
-        self.RescalingLayer = RescalingLayer # must map n_in to (n_out x n_out) matrix
+        self.RescalingLayer = RescalingLayer  # must map n_in to (n_out x n_out) matrix
 
         if not self.RescalingLayer == None:
-            self.rescale = RescalingLayer(**rescaling_layer_kwargs)  # construct as required
+            self.rescale = RescalingLayer(
+                **rescaling_layer_kwargs
+            )  # construct as required
         else:
             self.rescale = None
 
@@ -102,7 +109,7 @@ class FlexibleSequential(nn.Module):
         out = self.model(input)  # pass through to the stored net
         if not self.RescalingLayer is None:
             # The rescaling should take n_in -> a n_out x n_out matrix
-            # then out = model(input)*rescale(input) 
+            # then out = model(input)*rescale(input)
             return self.rescale(input, out)
         else:
             return out
