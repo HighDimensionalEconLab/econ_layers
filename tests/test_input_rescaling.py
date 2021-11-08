70cc853dffbe8cc0ef261df86e8449b6df2fd329
Jan Rosa <jan.rosa1993@gmail.com>
2021-11-08 12:16:11 -0800

    unit root test for inputrescaling

---
+++ tests/test_input_rescaling.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""Tests for InputRescaling layer"""
+
+import pytest
+import torch
+import numpy
+from econ_layers.layers import InputRescaling
+
+
+def test_input_rescaling():
+
+    x = torch.cartesian_prod(
+        torch.linspace(
+            0.0,
+            10.0,
+            steps=20,
+        ),
+        torch.linspace(
+            1.0,
+            3.0,
+            steps=5,
+        ),
+    )
+
+    input_layer_0 = InputRescaling(0)
+    input_layer_1 = InputRescaling(1)
+    y_1 = x[:, [0]]
+    y_0 = x[:, [1]]
+    input_mult_0 = input_layer_0(x, y_0)
+    input_mult_1 = input_layer_1(x, y_1)
+
+    assert torch.all(torch.isclose(input_mult_0, x[:, [0]] * x[:, [1]]))
+    assert torch.all(torch.isclose(input_mult_1, x[:, [0]] * x[:, [1]]))
