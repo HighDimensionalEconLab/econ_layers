14e71e4472ad89bcf192e133c31b26cd09ad9da1
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-11-18 00:01:38 -0800

    allow dict_to_cpu to have both floats and tensors

--- econ_layers/utilities.py
+++ econ_layers/utilities.py
@@ -4,4 +4,4 @@ def squeeze_cpu(x):
     return x.detach().squeeze().cpu().numpy() if torch.is_tensor(x) else x
   
 def dict_to_cpu(d):
-    return {name: squeeze_cpu(val).tolist() for (name, val) in d.items()}
+    return {name: squeeze_cpu(val).tolist() if torch.is_tensor(val) else val for (name, val) in d.items()}
