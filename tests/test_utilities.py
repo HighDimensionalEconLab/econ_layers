08f208d4fc3cfc020d7d53ce3b44841000083f37
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-11-18 00:05:01 -0800

    add float to test_dict_to_cpu()

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -25,11 +25,13 @@ def test_dict_to_cpu():
     d = {
         "simple_tensor": torch.tensor([7.1, 4.2]),
         "tensor_unsqueezed": torch.tensor([[[2.1, 4.3]], [[6.5, 8.7]]]),
-        "numpy_unsqueezed": np.array([[[2.1, 3.4]], [[5.6, 8.7]]])
+        "numpy_unsqueezed": np.array([[[2.1, 3.4]], [[5.6, 8.7]]]),
+        "a_float": 4.2
     }
     expected_dict = {
         "simple_tensor": [7.1, 4.2],
         "tensor_unsqueezed": [[2.1, 4.3], [6.5, 8.7]],
-        "numpy_unsqueezed": [[[2.1, 3.4]], [[5.6, 8.7]]]
+        "numpy_unsqueezed": [[[2.1, 3.4]], [[5.6, 8.7]]],
+        "a_float": 4.2
     }
     assert(d[key] == expected_dict[key] for key in d)
