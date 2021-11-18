136c91747df09b2749c8c5916bb3761cebb3b2cc
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-11-18 00:04:12 -0800

    passthrough in dict_to_cpu only passes floats

--- econ_layers/utilities.py
+++ econ_layers/utilities.py
@@ -4,4 +4,4 @@ def squeeze_cpu(x):
     return x.detach().squeeze().cpu().numpy() if torch.is_tensor(x) else x
   
 def dict_to_cpu(d):
-    return {name: squeeze_cpu(val).tolist() if torch.is_tensor(val) else val for (name, val) in d.items()}
+    return {name: val if isinstance(val, float) else squeeze_cpu(val).tolist() for (name, val) in d.items()}
