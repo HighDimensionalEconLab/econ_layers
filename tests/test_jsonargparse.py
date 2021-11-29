45f1cfda47abf8d6498dea6f708687106cb3d9e4
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-11-29 07:13:03 -0800

    change config access again

--- tests/test_jsonargparse.py
+++ tests/test_jsonargparse.py
@@ -38,4 +38,5 @@ def test_edit_layers():
             "rescaling_layer": None,
         },
     }
-    assert cli.config.as_dict()["model"]["ml_model"] == ml_model_expected
+    config = cli.config.as_dict()
+    assert config["model"]["ml_model"] == ml_model_expected
