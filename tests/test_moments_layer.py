0b3ea9ad2d4b03de7aab2e4756c2577ed491db14
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-12-30 18:05:15 -0800

    add moments layer test

---
+++ tests/test_moments_layer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""Tests for Moments layer"""
+
+import pytest
+import torch
+import numpy
+from econ_layers.layers import Moments
+
+
+def test_moments():
+    moments_layer = Moments(5)
+    x = torch.tensor([2.0, 1.5])
+    x_moments = moments_layer(x)
+    assert torch.all(
+        torch.isclose(
+            x_moments,
+            torch.tensor(
+                [[2.0, 4.0, 8.0, 16.0, 32.0], [1.5, 2.25, 3.375, 5.0625, 7.59375]]
+            ),
+        )
+    )
