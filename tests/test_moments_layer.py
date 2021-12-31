a6f1e8bac1e4cde1bf8461ff98c708db6c8a4e2a
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-12-30 19:31:34 -0800

    adjust moments test

--- tests/test_moments_layer.py
+++ tests/test_moments_layer.py
@@ -10,13 +10,13 @@ from econ_layers.layers import Moments
 
 def test_moments():
     moments_layer = Moments(5)
-    x = torch.tensor([2.0])
-    x_moments = moments_layer(x)
+    x = torch.tensor([2.0, 1.5])
+    x_moments = moments_layer(x.reshape([2, 1]))
     assert torch.all(
         torch.isclose(
             x_moments,
             torch.tensor(
-                [2.0, 4.0, 8.0, 16.0, 32.0]
+                [[2.0, 4.0, 8.0, 16.0, 32.0], [1.5, 2.25, 3.375, 5.0625, 7.59375]]
             ),
         )
     )
